a457d85eb4cf5ceb89f38b7a549825967dde7f73
Segev Shmueli <cj@segevs.com>
2025-05-22 12:37:36 -0400

    Release v0.1.9

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { CohostCheckoutProvider, CohostCheckoutContext, useCohostCheckout } from './context/CohostCheckoutContext';
 export { CohostEventProvider, useCohostEvent } from './context/CohostEventContext';
 export { CohostProvider, type CohostProviderProps } from './context/CohostContext';
-export { PaymentElementProvider } from './context/PaymentElementContext';
+export { PaymentElementProvider, usePaymentElement } from './context/PaymentElementContext';
 export { CreditCardInformation } from './lib/tokenizers/types';
 export { useCohost } from './hooks/useCohost';
